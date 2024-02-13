be376d3aace882cc7aac4d7555803b9aeb1c31be
ntsangarides <159533549+ntsangarides@users.noreply.github.com>
2024-02-13 21:12:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -42,22 +42,22 @@ st.markdown("""
 DB_PATH = (Path(__file__).parent / "data/patents.db").absolute()
 
 with st.sidebar:
-        
-        st.title("Ask me anything!")
-        
-        # Tools setup
-        llm = AzureChatOpenAI(
+    
+    st.title("Ask me anything!")
+    
+    # Tools setup
+    llm = AzureChatOpenAI(
         api_version=st.secrets["AZURE_OAI_API_VERSION"],
         deployment_name=st.secrets["AZURE_OAI_DEPLOYMENT"],
         model_name=st.secrets["AZURE_OAI_MODEL"],
         temperature=0, 
         streaming=True
-        )
-        # search = DuckDuckGoSearchAPIWrapper()
-        # llm_math_chain = LLMMathChain.from_llm(llm)
-        db = SQLDatabase.from_uri(f"sqlite:///{DB_PATH}")
-        db_chain = SQLDatabaseChain.from_llm(llm, db)
-        tools = [
+    )
+    # search = DuckDuckGoSearchAPIWrapper()
+    # llm_math_chain = LLMMathChain.from_llm(llm)
+    db = SQLDatabase.from_uri(f"sqlite:///{DB_PATH}")
+    db_chain = SQLDatabaseChain.from_llm(llm, db)
+    tools = [
         # Tool(
         #     name="Search",
         #     func=search.run,
@@ -73,18 +73,18 @@ with st.sidebar:
             func=db_chain.run,
             description="useful for when you need to answer questions about patents. Input should be in the form of a question containing full context",
         ),
-        ]
-        
-        # Initialize agent with prompt from hwchase17/react on LangChain Hub
-        react_agent = create_react_agent(llm, tools, prompt=hub.pull("hwchase17/react"))
-        mrkl = AgentExecutor(agent=react_agent, tools=tools)
-        
-        with st.form(key="form"):
+    ]
+    
+    # Initialize agent with prompt from hwchase17/react on LangChain Hub
+    react_agent = create_react_agent(llm, tools, prompt=hub.pull("hwchase17/react"))
+    mrkl = AgentExecutor(agent=react_agent, tools=tools)
+    
+    with st.form(key="form"):
         user_input = st.text_input("User query")
         submit_clicked = st.form_submit_button("Submit Question")
         
-        output_container = st.empty()
-        if with_clear_container(submit_clicked):
+    output_container = st.empty()
+    if with_clear_container(submit_clicked):
         output_container = output_container.container()
         output_container.chat_message("user").write(user_input)
         
@@ -96,6 +96,7 @@ with st.sidebar:
         answer = mrkl.invoke({"input": user_input}, cfg)
         
         answer_container.write(answer["output"])
+
 
 # Power BI report URL
 power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiMzU3YmZiOGEtNGExNi00OWQxLWI3OTAtMzA4MGFiOTlmODE3IiwidCI6ImIwMDM2N2UyLTE5M2EtNGY0OC05NGRlLTcyNDVkNDVjMDk0NyIsImMiOjh9"

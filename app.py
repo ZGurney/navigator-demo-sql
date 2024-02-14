dc7bbb96617030f59b10530e18ac9bbd9397aaa6
ntsangarides <159533549+ntsangarides@users.noreply.github.com>
2024-02-14 11:04:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.markdown(
     + "</styles>",
     unsafe_allow_html=True,
 )
-st.image(["logo.png", "logo_colour.png"], width=200)
+st.image(["Screenshot 2024-02-14 110312.png", "logo_colour.png"], width=200)
 
 # Set path to database /data/patents.db
 DB_PATH = (Path(__file__).parent / "data/patents.db").absolute()

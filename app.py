ec77f6be7c48fa2933ffe131a9cb8a8420ba1162
ntsangarides <159533549+ntsangarides@users.noreply.github.com>
2024-02-19 12:21:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -112,19 +112,6 @@ with col2:
             # Display the response
             with messages.chat_message("assistant"):
                 st.write(answer["output"])
-    
-    if with_clear_container(submit_clicked):
-        output_container = output_container.container()
-        output_container.chat_message("user").write(user_input)
-    
-        answer_container = output_container.chat_message("assistant", avatar="Screenshot 2024-01-04 144948.png")
-        st_callback = StreamlitCallbackHandler(answer_container)
-        cfg = RunnableConfig()
-        cfg["callbacks"] = [st_callback]
-    
-        answer = mrkl.invoke({"input": user_input}, cfg)
-    
-        answer_container.write(answer["output"])
 
 
 
